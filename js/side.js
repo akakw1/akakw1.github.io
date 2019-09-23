96b5ad3daf0f865e5a901557be7a5daa4d712ea8
akakw1 <44433637+akakw1@users.noreply.github.com>
2019-09-23 21:28:35 +0800

    Update side.js

--- js/side.js
+++ js/side.js
@@ -2,7 +2,7 @@ function Side() {
 	var obj = document.getElementById('side');
 	this.init = function() {
 		obj.innerHTML = [
-			"<span id='mainlink'><a href='windowsgui/index.html'>Windows GUI</a></span>"
+			"<span id='mainlink'><a href='windowsgui/index.html'>Windows GDI</a></span>"
 		].join('\n');
 	}
 }
